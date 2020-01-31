cd723a5376d10135d738d65fbe0c528f628685e3
Izueke Nonso <izuekenonsov@gmail.com>
2020-01-31 13:17:13 +0100

    Initial commit

---
+++ src/main/java/com/example/springsecurityjpa/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -0,0 +1,18 @@
+package com.example.springsecurityjpa.springsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.example.springsecurityjpa.springsecurityjpa.models.UserRepository;
+
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SpringSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaApplication.class, args);
+	}
+
+}
